7e9f2cc4fb916a37e059634835bba6e2554a326b
Louisy <135445087+LouisyDalch@users.noreply.github.com>
2026-03-31 17:06:17 -0300

    Update script.js
    função revelaTxt implementada

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ function menuShow(){
         document.querySelector('.icon').src = "imagens/close.svg"
     }
 }
+
+function revelaTxt(btn){
+    let sect = btn.parentElement;
+    let sectAmostra = sect.querySelector(".amostra");
+    let sectHide = sect.querySelector(".hide");
+
+    //para o programa pegar o status da tela, e não o q é atribuido no código
+    let status = window.getComputedStyle(sectHide).display !== "none"; //bool
+
+    sectHide.style.display = (status)? "none":"block";
+    sectAmostra.style.display = (sectHide.style.display == "none")? "block":"none";
+
+    btn.textContent = (btn.textContent == "Ver mais")? "Ver menos":"Ver mais";
+}
